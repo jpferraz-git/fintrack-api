56c628e69fe1d8f6bb5a2ab06c7a494ff695eaa1
jpferraz-git <jpferraz554@gmail.com>
2026-03-20 23:39:06 -0300

    models data type, getters and setters

--- backend/project/src/main/java/com/backend/project/domain/model/UserModel.java
+++ backend/project/src/main/java/com/backend/project/domain/model/UserModel.java
@@ -1,6 +1,7 @@
 package com.backend.project.domain.model;
 
 
+import java.time.Instant;
 import java.util.UUID;
 
 public class UserModel {
@@ -9,8 +10,54 @@ public class UserModel {
     private String name;
     private String email;
     private String password;
-    private String created_at;
-    private String updated_at;
+    private Instant created_at;
+    private Instant updated_at;
+
+    public UUID getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(UUID user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Instant created_at) {
+        this.created_at = created_at;
+    }
+
+    public Instant getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Instant updated_at) {
+        this.updated_at = updated_at;
+    }
 }
-
-
