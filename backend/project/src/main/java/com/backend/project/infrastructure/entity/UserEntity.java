429e1b23f98ccafd67774b9fa1f0000f3c93209d
jpferraz-git <jpferraz554@gmail.com>
2026-03-22 22:40:56 -0300

    update application.yml

--- backend/project/src/main/java/com/backend/project/infrastructure/entity/UserEntity.java
+++ backend/project/src/main/java/com/backend/project/infrastructure/entity/UserEntity.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
@@ -17,9 +18,11 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name="user_id", updatable = false, nullable = false)
-    private UUID user_id;
+    private UUID userId;
     private String name;
     @Email
     private String email;
     private String password;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
