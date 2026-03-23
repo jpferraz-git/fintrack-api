4216dd750857141677c8403d499c4ac649d90de4
jpferraz-git <jpferraz554@gmail.com>
2026-03-22 22:39:16 -0300

    add entity scan

--- backend/project/src/main/java/com/backend/project/ProjectApplication.java
+++ backend/project/src/main/java/com/backend/project/ProjectApplication.java
@@ -2,8 +2,10 @@ package com.backend.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.backend.project.infrastructure.entity")
 public class ProjectApplication {
 
 	public static void main(String[] args) {
