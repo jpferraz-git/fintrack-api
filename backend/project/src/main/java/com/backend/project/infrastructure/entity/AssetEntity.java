bd5b4bb71bb5d08831b5354a49b34453fe6377e8
jpferraz-git <jpferraz554@gmail.com>
2026-03-23 11:10:31 -0300

    add fileds to asset entity

--- backend/project/src/main/java/com/backend/project/infrastructure/entity/AssetEntity.java
+++ backend/project/src/main/java/com/backend/project/infrastructure/entity/AssetEntity.java
@@ -1,5 +1,34 @@
 package com.backend.project.infrastructure.entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "assets")
+@Getter
+@Setter
 public class AssetEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID assetId;
+    private String ticker;
+    private String assetType;
+    private String companyName;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 }
--- backend/project/src/main/java/com/backend/project/infrastructure/entity/UserEntity.java
+++ backend/project/src/main/java/com/backend/project/infrastructure/entity/UserEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -23,6 +25,12 @@ public class UserEntity {
     @Email
     private String email;
     private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
     private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
     private Instant updatedAt;
 }
