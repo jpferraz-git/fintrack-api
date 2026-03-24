86e819ca334427eabbe3c0b403999f13c298e11b
jpferraz-git <jpferraz554@gmail.com>
2026-03-24 13:52:39 -0300

    /api endpoint  now its universal

--- backend/project/src/main/java/com/backend/project/interfaces/controllers/BatchController.java
+++ backend/project/src/main/java/com/backend/project/interfaces/controllers/BatchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/batch")
+@RequestMapping("/batch")
 public class BatchController {
 
     private final BatchService batchService;
