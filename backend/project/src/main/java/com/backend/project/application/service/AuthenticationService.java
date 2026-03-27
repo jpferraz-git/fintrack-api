7db97db4bc702bbe70802de6be5e498a2b80bd7c
jpferraz-git <jpferraz554@gmail.com>
2026-03-27 11:17:14 -0300

    removing business logic from authentication controller

---
+++ backend/project/src/main/java/com/backend/project/application/service/AuthenticationService.java
@@ -0,0 +1,55 @@
+package com.backend.project.application.service;
+
+import com.backend.project.domain.repository.RoleRepository;
+import com.backend.project.domain.repository.UserRepository;
+import com.backend.project.exception.EmailAlreadyInUseException;
+import com.backend.project.infrastructure.entity.UserEntity;
+import com.backend.project.interfaces.dto.authentication.AuthenticationDTO;
+import com.backend.project.interfaces.dto.authentication.LoginResponseDTO;
+import com.backend.project.interfaces.dto.register.RegisterDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenService tokenService;
+
+    private final RoleRepository roleRepository;
+
+    public AuthenticationService(UserRepository userRepository, AuthenticationManager authenticationManager, TokenService tokenService, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.roleRepository = roleRepository;
+    }
+
+    public LoginResponseDTO login(AuthenticationDTO dto){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        var token = tokenService.generateToken((UserEntity) auth.getPrincipal());
+
+        return new LoginResponseDTO(token);
+    }
+    
+    public String register(RegisterDTO dto){
+        if (this.userRepository.findByEmail(dto.email()) != null) {
+            throw new EmailAlreadyInUseException(dto.email());
+        }
+        
+        String encryptedPassword = new BCryptPasswordEncoder(12).encode(dto.password());
+        UserEntity newUser = new UserEntity(
+                dto.email(),
+                encryptedPassword,
+                roleRepository.findByName(dto.role())
+        );
+        System.out.println(newUser);
+        this.userRepository.create(newUser);
+        return "User registered successfully";
+    }
+    
+}
--- backend/project/src/main/java/com/backend/project/interfaces/controllers/AuthenticationController.java
+++ backend/project/src/main/java/com/backend/project/interfaces/controllers/AuthenticationController.java
@@ -1,64 +1,34 @@
 package com.backend.project.interfaces.controllers;
 
-import com.backend.project.application.service.TokenService;
-import com.backend.project.domain.repository.RoleRepository;
-import com.backend.project.domain.repository.UserRepository;
-import com.backend.project.infrastructure.entity.UserEntity;
+import com.backend.project.application.service.AuthenticationService;
 import com.backend.project.interfaces.dto.authentication.AuthenticationDTO;
 import com.backend.project.interfaces.dto.authentication.LoginResponseDTO;
 import com.backend.project.interfaces.dto.register.RegisterDTO;
+import com.backend.project.interfaces.swagger.AuthenticationControllerSwagger;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthenticationController {
+public class AuthenticationController implements AuthenticationControllerSwagger {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationService authenticationService;
 
-    @Autowired
-    private TokenService tokenService;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO dto){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.generateToken((UserEntity) auth.getPrincipal());
-
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO dto) {
+        return ResponseEntity.ok().body(authenticationService.login(dto));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody @Valid RegisterDTO dto){
-        if (this.userRepository.findByEmail(dto.email()) != null) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already in use");
-        }
-        //TODO Jogar isso aqui para a service
-        String encryptedPassword = new BCryptPasswordEncoder(12).encode(dto.password());
-        UserEntity newUser = new UserEntity(
-                dto.email(),
-                encryptedPassword,
-                roleRepository.findByName(dto.role())
-        );
-        System.out.println(newUser);
-        this.userRepository.create(newUser);
-        return ResponseEntity.ok().body("User registered successfully");
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterDTO dto) {
+        return ResponseEntity.ok().body(authenticationService.register(dto));
     }
 }
