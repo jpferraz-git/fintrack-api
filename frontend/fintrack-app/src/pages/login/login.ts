8de59dd5ac9ebcc6953c8afa525959cbab542ba7
jpferraz-git <jpferraz554@gmail.com>
2026-03-31 13:22:49 -0300

    login page

---
+++ frontend/fintrack-app/src/pages/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthLayout } from '../../components/auth-layout/auth-layout';
+import { AuthCard } from '../../components/auth-card/auth-card';
+import { FormInput } from '../../components/form-input/form-input';
+import { CtaButton } from '../../components/cta-button/cta-button';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [AuthLayout, AuthCard, FormInput, CtaButton, RouterLink],
+  templateUrl: './login.html'
+})
+export class LoginPage {}
