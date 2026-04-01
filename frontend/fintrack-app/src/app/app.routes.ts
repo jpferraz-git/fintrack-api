97d147419830b379980ba462c6ae7fe6fc0f90c8
jpferraz-git <jpferraz554@gmail.com>
2026-04-01 09:58:34 -0300

    update app.routes

--- frontend/fintrack-app/src/app/app.routes.ts
+++ frontend/fintrack-app/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+import { AlertsPage } from '../pages/alerts/alerts';
+import { DashboardPage } from '../pages/dashboard/dashboard';
 import { LoginPage } from '../pages/login/login';
+import { PortfolioPage } from '../pages/portfolio/portfolio';
 import { SignupPage } from '../pages/signup/signup';
 
 export const routes: Routes = [
-	{ path: '', pathMatch: 'full', redirectTo: 'login' },
+	{ path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+	{ path: 'dashboard', component: DashboardPage },
+	{ path: 'portfolio', component: PortfolioPage },
+	{ path: 'alerts', component: AlertsPage },
 	{ path: 'login', component: LoginPage },
 	{ path: 'signup', component: SignupPage },
-	{ path: '**', redirectTo: 'login' }
+	{ path: '**', redirectTo: 'dashboard' }
 ];
