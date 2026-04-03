6372f2fea543dadbbf1e577811dc4c17df953a1b
jpferraz-git <jpferraz554@gmail.com>
2026-04-03 13:08:08 -0300

    signup working :)

--- frontend/fintrack-app/src/pages/signup/signup.component.ts
+++ frontend/fintrack-app/src/pages/signup/signup.component.ts
@@ -1,32 +1,54 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { AuthLayout } from '../../components/auth-layout/auth-layout.component';
 import { AuthCard } from '../../components/auth-card/auth-card.component';
 import { FormInput } from '../../components/form-input/form-input.component';
 import { CtaButton } from '../../components/cta-button/cta-button.component';
+import { SignupService } from '../../app/services/signup.service';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup-page',
-  imports: [NgIf, AuthLayout, AuthCard, FormInput, CtaButton, RouterLink],
+  imports: [AuthLayout, AuthCard, FormInput, CtaButton, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
+
+
 export class SignupPage {
+  name = '';
+  email = '';
   password = '';
   confirmPassword = '';
   showPasswordMismatchWarning = false;
 
+  constructor(
+    private router: Router,
+    private singupService: SignupService,
+  ) {}
+
   onSignupSubmit(event: Event): void {
     event.preventDefault();
-
     const passwordsMatch = this.password === this.confirmPassword;
     this.showPasswordMismatchWarning = !passwordsMatch;
-
+    
     if (!passwordsMatch) {
-      return;
+      return
     }
+    this.singupService.singUp({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    }).subscribe ({
+      next: () => {
+        console.log(`User with ${this.email} created` )
+          this.router.navigate(['/login'])
+      },
+      complete() {
+  
+      },
+    })
 
-    // TODO: seguir com o fluxo de cadastro quando a integração estiver pronta.
   }
+
 }
