a701c1004d73024020123f00c503858a3ac5c945
jpferraz-git <jpferraz554@gmail.com>
2026-04-03 22:40:17 -0300

    market-data kpi using angular timer for bitcoin cripto

--- frontend/fintrack-app/src/components/dashboard/dashboard-market-cards/dashboard-market-cards.component.ts
+++ frontend/fintrack-app/src/components/dashboard/dashboard-market-cards/dashboard-market-cards.component.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, catchError, exhaustMap, of, takeUntil, timer } from 'rxjs';
+import { MarketDataService } from '../../../app/services/market-data.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-market-cards',
   imports: [],
-  templateUrl: './dashboard-market-cards.component.html',
+  standalone: true,
+  templateUrl: './dashboard-market-cards.component.html', 
   styleUrl: './dashboard-market-cards.component.css'
 })
-export class DashboardMarketCards {}
+export class DashboardMarketCards implements OnInit, OnDestroy {
+
+  private readonly destroy$ = new Subject<void>();
+  btcPrice = '--';
+  btcVolume = 'Vol: 0 USDT';
+  btcDelta = 'LIVE';
+  btcTrendUp = true;
+  loadingBtc = false;
+
+constructor(
+  private marketDataService: MarketDataService,
+  private cd: ChangeDetectorRef
+) {}
+
+  ngOnInit(): void {
+    this.startBtcPolling();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private startBtcPolling(): void {
+    this.loadingBtc = true;
+
+    timer(0, 2000)
+      .pipe(
+        exhaustMap(() => this.marketDataService.getPrice('BTCUSDT')
+          .pipe(catchError(() => of(null)))),
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        next: (ticker) => {
+          this.loadingBtc = false;
+          console.log('BTC Ticker:', ticker);
+
+          if (!ticker) {
+            this.btcDelta = 'OFFLINE';
+            this.btcPrice = '--';
+            this.btcVolume = 'Vol: --';
+            this.btcTrendUp = true;
+            return;
+          }
+
+          this.btcDelta = 'LIVE';
+          this.btcPrice = String(ticker.price);
+          this.cd.markForCheck();
+        }
+      });
+  }
+}
