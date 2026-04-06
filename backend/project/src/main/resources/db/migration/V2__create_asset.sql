4c6db8c58d3d7817bbcdc582f40f3bb64b33eb55
jpferraz-git <jpferraz554@gmail.com>
2026-04-06 11:32:33 -0300

    removing price field from assets table

--- backend/project/src/main/resources/db/migration/V2__create_asset.sql
+++ backend/project/src/main/resources/db/migration/V2__create_asset.sql
@@ -3,6 +3,5 @@ CREATE TABLE assets (
     symbol VARCHAR(15) NOT NULL UNIQUE,
     type VARCHAR(50) NOT NULL,
     quantity DECIMAL(18,8) NOT NULL,
-    price DECIMAL(18,2) NOT NULL,
     avg_price DECIMAL(18,2) NOT NULL
 );
