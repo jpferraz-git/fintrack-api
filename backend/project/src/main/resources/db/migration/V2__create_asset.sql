4d6a089406468fbac0cad10fee49e65aea938dee
jpferraz-git <jpferraz554@gmail.com>
2026-04-06 12:30:22 -0300

    remove UNIQUE constraint from symbol field in assets table

--- backend/project/src/main/resources/db/migration/V2__create_asset.sql
+++ backend/project/src/main/resources/db/migration/V2__create_asset.sql
@@ -1,6 +1,6 @@
 CREATE TABLE assets (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    symbol VARCHAR(15) NOT NULL UNIQUE,
+    symbol VARCHAR(15) NOT NULL,
     type VARCHAR(50) NOT NULL,
     quantity DECIMAL(18,8) NOT NULL,
     avg_price DECIMAL(18,2) NOT NULL
