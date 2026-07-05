785ef245f1ee5d1f89c37661b128d09b66bd1b6b
jpferraz-git <jpferraz554@gmail.com>
2026-07-05 14:52:02 -0300

    refactor(dto): simplificar mappers da Binance removendo dependencias de models

--- backend/project/src/main/java/com/backend/project/domain/repository/BinanceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.project.domain.repository;
-
-import com.backend.project.infrastructure.entity.BinancePriceEntity;
-
-public interface BinanceRepository {
-
-    BinancePriceEntity createBinancePrice(BinancePriceEntity entity);
-}
