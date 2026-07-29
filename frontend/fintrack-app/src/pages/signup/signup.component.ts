2fc0e74d8cd148b53a864ebea6fdd13e6ebf0a22
jpferraz-git <jpferraz554@gmail.com>
2026-07-29 12:55:33 -0300

    refactor(signup): migrate signup form to reactive forms with validation logic

--- frontend/fintrack-app/src/pages/signup/signup.component.ts
+++ frontend/fintrack-app/src/pages/signup/signup.component.ts
@@ -6,59 +6,67 @@ import { CtaButton } from '../../components/cta-button/cta-button.component';
 import { SignupService } from '../../app/services/signup.service';
 import { Router, RouterLink } from '@angular/router';
 import { UtilsService } from '../../app/services/utils.service';
-
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
-  imports: [AuthLayout, AuthCard, FormInput, CtaButton, RouterLink],
+  imports: [ReactiveFormsModule, AuthLayout, AuthCard, FormInput, CtaButton, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-
-
 export class SignupPage {
-  name = '';
-  email = '';
-  password = '';
-  confirmPassword = '';
+  signupForm: FormGroup;
   showPassword = false;
   showConfirmPassword = false;
   passwordValidationError = '';
   showPasswordMismatchWarning = false;
 
   constructor(
+    private fb: FormBuilder,
     private router: Router,
     private singupService: SignupService,
     private utilsService: UtilsService
-  ) {}
+  ) {
+      this.signupForm = this.fb.group({
+          name: ['', [Validators.required, Validators.minLength(2)]],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [
+              Validators.required,
+              Validators.minLength(8),
+              Validators.pattern("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$")
+          ]],
+          confirmPassword: ['', [Validators.required]]
+      });
+  }
 
   onSignupSubmit(event: Event): void {
     event.preventDefault();
+    if (this.signupForm.invalid) {
+        this.signupForm.markAllAsTouched();
+        return;
+    }
+
     this.passwordValidationError = '';
     this.showPasswordMismatchWarning = false;
 
-    const passwordValidationError = this.utilsService.validatePassword(this.password);
-    if (passwordValidationError) {
-      this.passwordValidationError = passwordValidationError;
-      return;
-    }
+    const { name, email, password, confirmPassword } = this.signupForm.value;
 
-    const passwordsMatch = this.password === this.confirmPassword;
+    const passwordsMatch = password === confirmPassword;
     this.showPasswordMismatchWarning = !passwordsMatch;
     
     if (!passwordsMatch) {
       return
     }
+    
     this.singupService.singUp({
-      name: this.name,
-      email: this.email,
-      password: this.password,
+      name,
+      email,
+      password,
     }).subscribe ({
       next: () => {
           this.router.navigate(['/login'])
       }
     })
-
   }
 
   togglePasswordVisibility(): void {
